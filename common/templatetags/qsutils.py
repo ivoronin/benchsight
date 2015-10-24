8b60640df5ca4e1f21554d26d3f2768687547c2e
Ilya Voronin <ivoronin@gmail.com>
2015-10-24 18:01:25 +0300

    Remove print()

--- common/templatetags/qsutils.py
+++ common/templatetags/qsutils.py
@@ -5,6 +5,5 @@ register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def qscut(context, cut):
-    print(type(context))
     items = context['request'].GET.items()
     return urlencode({k:v for k,v in items if k!=cut})
